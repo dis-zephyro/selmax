39f59c959a4d41a70705c18486039af3b919b4e2
dis-zephyro <zephyro@yandex.ru>
2015-09-20 14:09:36 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -14,6 +14,46 @@ $(".btn-popup").fancybox({
 });
 
 
+$(document).ready(function() {
+
+    $('.btn-submit').click(function() {
+
+        $('body').find('form:not(this)').children('label').removeClass('red'); //удаление всех сообщение об ошибке(валидатора)
+        var answer = checkForm($(this).closest('form').get(0)); //ответ от валидатора
+        if(answer != false)
+        {
+            var $form = $(this).closest('form'),
+                name    =     $('input[name="name"]', $form).val(),
+                phone   =     $('input[name="phone"]', $form).val(),
+                city    =     $('input[name="city"]', $form).val(),
+                org     =     $('input[name="organisation"]', $form).val(),
+                message =     $('input[name="city"]', $form).val();
+            console.log(name, phone);
+            $.ajax({
+                type: "POST",
+                url: "form-handler.php",
+                data: {name: name, phone: phone, city: city, org: org, message: message}
+            }).done(function(msg) {
+                console.log(name, phone, city, org, org);
+                $('form').find('input[type=text], textarea').val('');
+                console.log('удачно');
+                $.fancybox(
+                    '<div class="done">'+ '<span class="done-title">Спасибо, Ваша заявка принята!</span><br/>В скором времени с вами свяжутся наши менеджеры' +'</div>',
+                    {
+                        'autoDimensions'  : false,
+                        'padding': 0,
+                        'transitionIn'    : 'none',
+                        'transitionOut'   : 'none',
+                        'closeBtn' : false
+                    }
+                );
+                setTimeout("$.fancybox.close()", 3000);
+            });
+        }
+    });
+});
+
+
 
 // Подключние Яндекс-Карты
 
